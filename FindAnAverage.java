e62b2ccc6c06b2dcebee3f677a8bfb7ee48db4cb
Neil Campbell <neildcampbell@hotmail.com>
2018-04-12 12:00:24 +0100

    Update and rename Main.java to FindAnAverage.java

--- FindAnAverage.java
+++ FindAnAverage.java
@@ -1,4 +1,3 @@
-package com.pluralsight.calcengine;
 
 import java.util.Scanner;
 
@@ -7,17 +6,42 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        //initiate legth of array my numbers and link to method getNumbers
         int[] myNumbers = getNumbers(5);
-
+        // lists out numbers typed in to console
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println("The number entered in was " + myNumbers[i]);
         }
+        //initiate variables
         double myMean = myAnswer(0);
         int myScore=0;
+        //call method to check if answer is correct
         checkAnswer(myMean, myNumbers, myScore);
         }
-
+        //print out numbers entered
+    private static int[] getNumbers(int number) {
+        System.out.println("Enter in " + number + " new numbers");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+    //accept given answer using scanner and send answer to myMean
+    private static double myAnswer(double answer){
+        System.out.println("Calculate the mean for your numbers: ");
+        answer = scanner.nextDouble();
+        return answer;
+    }
+    // calculate the mean average of numbers given
+    private static double getAverage(int []array){
+        int sum = 0;
+        for(int i = 0;i<array.length;i++){
+            sum+=array[i];
+        }
+        return (double)sum / (double)array.length;
+        }
+    //check if given answer is correct and calculate score
     private static double checkAnswer(double myMean, int[] myNumbers, int myScore){
 
         if(myMean==getAverage(myNumbers)){
@@ -28,28 +52,6 @@ public class Main {
         }
         System.out.println("The mean is " + getAverage(myNumbers));
         return getAverage(myNumbers);
-        }
-
-    private static int[] getNumbers(int number) {
-        System.out.println("Enter in " + number + " new numbers");
-        int[] values = new int[number];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = scanner.nextInt();
-        }
-        return values;
-    }
-    private static double myAnswer(double answer){
-        System.out.println("Calculate the mean for your numbers: ");
-        answer = scanner.nextDouble();
-        return answer;
-    }
-    private static double getAverage(int []array){
-        int sum = 0;
-        for(int i = 0;i<array.length;i++){
-            sum+=array[i];
-
-        }
-        return (double)sum / (double)array.length;
         }
     }
 
